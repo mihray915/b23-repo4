af4e39d539b38b197b371d7e6d3c7d567a6ac641
mihray <mihray0126@gmail.com>
2023-04-22 13:18:23 -0400

    Create clone note.txt.c

---
+++ clone note.txt.c
@@ -0,0 +1,7 @@
+i just creted emty remote repository from GitHub.com account by clicking on the plus icon on top right corner and new resitory and give a name and click create  
+
+and cloned this repository by clicking on setup in GitHub desktop and (optionally select the location)
+click on the clone
+
+it clone our remote repo to local so we can start 
+working loacally
